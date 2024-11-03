8f1d85bba181862fb0eacb0d81223999d2dc5fdf
Eduardo Vedes <eduardo.vedes@gmail.com>
2024-11-03 12:35:21 +0000

    chore: add fonts

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMonoNerdFont")
+config.font_size = 12
+config.line_height = 1.30
+
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+
+config.color_scheme = "kanagawabones"
+
+config.window_background_opacity = 1
+
+config.window_close_confirmation = "NeverPrompt"
+config.adjust_window_size_when_changing_font_size = false
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.keys = {
+	-- Scroll Up/Down with keyboard
+	{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-0.5) },
+	{ key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(0.5) },
+	-- Disable default new tab keybinding
+	{ key = "t", mods = "CMD", action = act.DisableDefaultAssignment },
+}
+
+return config
--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -1,9 +1,2 @@
 # ALIAS
 source $HOME/.arch-dotfiles/zshrc/.alias
-
-# bun completions
-[ -s "/home/edo/.bun/_bun" ] && source "/home/edo/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
